cd28ae0f39c9521dcd513b30fd0aad2fb83b2488
Ocedy16 <118558735+Ocedy16@users.noreply.github.com>
2023-12-10 21:44:00 +0000

    The second part of the task. To be debugged

--- Day1.py
+++ Day1.py
@@ -1,3 +1,4 @@
+
 file_url = 'https://raw.githubusercontent.com/Ocedy16/AdventOfCode/main/AoC_Day1'
 import urllib.request
 
@@ -6,14 +7,37 @@ lines = f.readlines()
 f.close()
 
 total = 0
-for line in lines : 
+convert = {"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
+for line in lines :
     line = line.decode('utf-8')
-    for letter in line : 
-        if letter in "0123456789":
+    index_min = len(line)
+    index_max = 0
+    number_min = ""
+    number_max = ""
+    number_names = list(convert.keys())
+    for number in number_names:
+        if number in line : 
+            if line.find(number) < index_min : 
+                index_min = line.find(number)
+                number_min = number
+            if line.find(number) > index_max : 
+                index_max = line.find(number)  
+                number_max = number
+            
+    passed = False
+    for letter in line[0:index_min] : 
+        if letter.isdigit() :
             total += int(letter)*10
+            passed = True
             break
-    for letter in line [::-1]:
-        if letter in "0123456789":
+    if not passed : 
+        total += convert[number_min]*10
+    passed = False 
+    for letter in line [index_max:] [::-1]:
+        if letter.isdigit() :
             total += int(letter)
+            passed = True
             break
+    if not passed : 
+        total += convert[number_max]
 print(total)
